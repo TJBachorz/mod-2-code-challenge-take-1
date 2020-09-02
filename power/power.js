a90115df9b925c288876e56b162fd1c4c906afaa
TJBachorz <TJBachorz@gmail.com>
2020-09-02 15:49:42 -0600

    fixes more issues

--- power/power.js
+++ power/power.js
@@ -7,14 +7,25 @@ fetch(`http://localhost:3000/powers/${powerId}`)
 
 function showHolders(power) {
     const body = document.querySelector('body')
+    makeHead(power, body)
+    makeDesc(power, body)
+    getHeroineNames(power, body)
+}
+
+
+function makeHead(power, body) {
     const head = document.createElement('h2')
     head.textContent = power.name
     body.appendChild(head)
+}
 
+function makeDesc(power, body) {
     const desc = document.createElement('p')
     desc.textContent = power.description
     body.appendChild(desc)
+}
 
+function getHeroineNames(power, body) {
     const namesHeader = document.createElement('h3')
     namesHeader.textContent = "Holders of this power:"
     body.appendChild(namesHeader)
@@ -22,7 +33,7 @@ function showHolders(power) {
     console.log(heroines)
     heroines.map(heroine => {
         const name = document.createElement('p')
-        name.innerHTML = `<a href="http://localhost:3003/heroine/heroine.html?heroine_id=${heroine.id}">${heroine.name}</a>`
+        name.innerHTML = `<a href="http://localhost:3001/heroine/heroine.html?heroine_id=${heroine.id}">${heroine.name}</a>`
         body.appendChild(name)
     })
 }
